77ca3693aaec1f704ee20c449e680cfb68418fe8
Daniel Tangoletto <daniel.tangoletto@live.fr>
2024-09-11 23:44:32 +0200

    test routes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/portfolio_react" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/projects" element={<Projects />} />
